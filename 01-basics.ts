e4923f970e38afd29f6d7130b32054afb5ce3da9
Ellissea Montes <montes.ellissea.pasillos@gmail.com>
2026-03-27 15:15:40 +0800

    Done playing and updating the 01-basic.ts file

--- 01-basics.ts
+++ 01-basics.ts
@@ -1,20 +1,24 @@
 // console.log("Hello World!")
 
-let userName: string = "Elmer";
-let userAge = 31;
+let userName: string = "Ellissea";
+let userAge = 24;
 
 // ...
 
-userAge = "31"
+userAge =24
 
 console.log(`Username - ${userName}`)
 console.log(`Age - ${userAge}`)
 
-function add(a: number, b = 5) {
+function add(a: number, b = 7):number {
     return a + b
 }
 
-add(10)
-// add('10')
-add(10, 6)
-// add(10, '6')
+add(8)
+// add(9)
+add(9)
+// add(9)
+
+//Show Value
+console.log(add(8))
+console.log(add(9))
