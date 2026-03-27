55e07952a378e3a32af8b204f2e96e92dd5bfe7e
Ellissea Montes <montes.ellissea.pasillos@gmail.com>
2026-03-27 16:16:58 +0800

    Done playing and updating the 05-enum.ts

--- 05-enum.ts
+++ 05-enum.ts
@@ -1,14 +1,11 @@
 (() => {
     enum Role {
-        // SuperAdmin,
-        // SuperAdmin = 1,
         SuperAdmin = 'SuperAdmin',
-        // Teacher,
         Teacher = 'Teacher',
-        // Student,
         Student = 'Student',
     }
 
-    // let userRole: Role = 0;
     let userRole: Role = Role.SuperAdmin;
-})
+
+    console.log(userRole);
+})();
