7b6536e5f2dd6af402e30f91454fe52624475eb8
Ellissea Montes <montes.ellissea.pasillos@gmail.com>
2026-03-27 15:36:06 +0800

    Done playing and updating the 02-vanilla-js-types.js

--- 02-vanilla-js-types.js
+++ 02-vanilla-js-types.js
@@ -1,3 +1,11 @@
-let userName1 = "Elmer";
+let userName1 = "Ellissea";
+let userAge1 = 24;
 
-console.log(typeof userName1)
+// reassign values (optional, but correct)
+userName1 = "Ryan";
+userAge1 = 27;
+
+console.log(typeof userName1);
+console.log(typeof userAge1);
+
+// loosely typed language
